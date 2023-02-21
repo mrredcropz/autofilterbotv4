20c13a14a614470ef0d82b021d93ac182945059c
mrredcropz <125983998+mrredcropz@users.noreply.github.com>
2023-02-21 21:34:43 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    ' 🌹 Our Group 🌹', url="https://t.me/movi"
                                 )
                         ]
                     ]
